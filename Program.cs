ec797b983b410a710c71e051b39b0b44a937f71c
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-25 19:59:38 -0300

    finalizar progamra

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+
+string senhaDigitada;
+string senhaEsperada = "110209";
+
+Console.Clear();
+
+Console.WriteLine("digite sua senha");
+senhaDigitada = Console.ReadLine()!;
+
+if(senhaDigitada == senhaEsperada)
+{ Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("olá usuário login com sucesso");
+}
+else
+
+{  Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("senha incorreta"); }
+
+Console.ResetColor();
